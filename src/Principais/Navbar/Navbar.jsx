4ed3b7db26fbd49bbea16d7e53e4da1e6e19a311
Nickolas Amaral <125821680+nickolasamaral03@users.noreply.github.com>
2024-04-11 14:20:03 -0300

    Melhoramos a navbar e aumentamos a fonte para celulares

--- src/Principais/Navbar/Navbar.jsx
+++ src/Principais/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import sino2 from '../../assets/tools_14030963.png'
 import profile from '../../assets/profile_netlfix.png'
+import N from '../../assets/letra_netflix.png'
 import './Navbar.css'
 import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
@@ -18,33 +19,33 @@ const handleSubmit = (e) => {
 }
 
   return (
+    <>
     <div className="toda_navbar">
 
-        <div className="netflix_logo">
-             <Link to="/Browser" style={{ textDecoration: 'none'}}><h3 className='netflix'>NETFLIX</h3></Link>
-        </div>
-        <div className="navegacoes">
-           <Link to="/Browser" style={{ textDecoration: 'none', backgroundColor: 'transparent' }}><p className='p_inicio'>Início</p></Link>
-           <Link to="/series" style={{ textDecoration: 'none', backgroundColor: 'transparent' }}><p>Séries</p></Link>
-           <Link to="/filmes" style={{ textDecoration: 'none', backgroundColor: 'transparent' }}><p>Filmes</p></Link>
-           <Link to="/Bombando" style={{ textDecoration: 'none', backgroundColor: 'transparent' }}><p>Bombando</p></Link>
-           
-        </div>
+      <div className="netflix_logo">
+        <Link to="/Browser" style={{ textDecoration: 'none' }}><h3 className='netflix'>NETFLIX</h3></Link>
+        <Link to="/Browser" style={{ textDecoration: 'none' }}><img src={N} alt="" className='N' /></Link>
 
-        <div className="last_section">
-          <form onSubmit={handleSubmit}>  {/*ação de apertar no enter e executar */}
-            <input type="text" placeholder='Busque um filme' 
-              onChange={(e) => setSearch(e.target.value)} //pegando valor do input
-              value={search}
-              className='input_search'
-            />
-            </form>
-            <Link to="/series" style={{ textDecoration: 'none', backgroundColor: 'transparent' }}><p className='infantil'>Infantil</p></Link>
-            <img src={sino2} alt="sino" className='sino'/>
-            <span className='number_random'>2</span>
-            <Link to="/" style={{ textDecoration: 'none', backgroundColor: 'transparent' }}><img src={profile} alt="perfil" className='perfil' /></Link>
-        </div>
-    </div>
+    </div><div className="navegacoes">
+        <Link to="/Browser" style={{ textDecoration: 'none', backgroundColor: 'transparent' }}><p className='p_inicio'>Início</p></Link>
+        <Link to="/series" style={{ textDecoration: 'none', backgroundColor: 'transparent' }}><p>Séries</p></Link>
+        <Link to="/filmes" style={{ textDecoration: 'none', backgroundColor: 'transparent' }}><p>Filmes</p></Link>
+        <Link to="/Bombando" style={{ textDecoration: 'none', backgroundColor: 'transparent' }}><p>Bombando</p></Link>
+
+      </div><div className="last_section">
+        <form onSubmit={handleSubmit}>  {/*ação de apertar no enter e executar */}
+          <input type="text" placeholder='Busque um filme'
+            onChange={(e) => setSearch(e.target.value)} //pegando valor do input
+            value={search}
+            className='input_search' />
+        </form>
+        <Link to="/series" style={{ textDecoration: 'none', backgroundColor: 'transparent' }}><p className='infantil'>Infantil</p></Link>
+        <img src={sino2} alt="sino" className='sino' />
+        <span className='number_random'>2</span>
+        <Link to="/" style={{ textDecoration: 'none', backgroundColor: 'transparent' }}><img src={profile} alt="perfil" className='perfil' /></Link>
+      </div>
+      </div>
+  </>
   )
 }
 
